c92634001601b2cdd4b9eae3ddb463912dfd2a76
Owlsly <stefan.stanimirovic09@gmail.com>
2026-02-13 05:03:53 +0100

    new updates

--- flake.nix
+++ flake.nix
@@ -3,12 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     stylix.url = "github:danth/stylix";
   };
 
@@ -21,7 +19,6 @@
       modules = [
         ./configuration.nix
         stylix.nixosModules.stylix
-
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
