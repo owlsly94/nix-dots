63c69f8ed84bbfcbe343933a886e09217af63efb
Owlsly <stefan.stanimirovic09@gmail.com>
2026-02-27 08:04:52 +0100

    cleaning

--- modules/system/overlays.nix
+++ modules/system/overlays.nix
@@ -1,9 +1,6 @@
 { pkgs-unstable, ... }:
 
 {
-  # Use unstable versions of fast-moving packages for stability and features
-  # This overlay allows us to maintain static versions for most packages
-  # while getting timely updates for apps that benefit from newer releases
   nixpkgs.overlays = [
     (final: prev: {
       lutris = pkgs-unstable.lutris;
